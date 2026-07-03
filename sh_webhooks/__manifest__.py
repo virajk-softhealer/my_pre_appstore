15c8b71264607837cd2b591b0d7f3757adacfd7f
virajk-softhealer <virajk@softhealer.com>
2026-07-03 12:01:55 +0530

    [UPDATE] manifest file.

--- sh_webhooks/__manifest__.py
+++ sh_webhooks/__manifest__.py
@@ -10,7 +10,7 @@
     'summary': 'All in One Webhooks, Custom Webhooks, Incoming Webhooks, Outgoing Webhooks, Webhook Integration, Webhook Automation, Webhook Logging, Webhook Analytics Odoo',
     'description': """This module allows you to create custom webhooks for any model in Odoo. You can define HTTP callbacks that fire when records are created, updated, or on a schedule. It supports Python scripted adaptations for URL, headers, and payload.""",
     'version': '0.0.1',
-    'depends': ['base', 'base_automation', 'mail'],
+    'depends': ['base_automation'],
     'data': [
         'security/ir.model.access.csv',
         'views/sh_webhook_action_views.xml',
