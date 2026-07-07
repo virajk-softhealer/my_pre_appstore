9e26b3f5c7745121165d5b10db7af2d2d0dbb2ca
virajk-softhealer <virajk@softhealer.com>
2026-07-07 12:12:35 +0530

    [UPDATE] sh_webhooks

--- sh_webhooks_v19/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) Softhealer Technologies Pvt. Ltd.
-
-from . import sh_webhook_action
-from . import sh_webhook_log
-from . import sh_webhook_test_wizard
-from . import sh_webhook_auth
-from . import sh_webhook_incoming
-from . import ir_actions
